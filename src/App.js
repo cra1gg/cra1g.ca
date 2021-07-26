064e46ed0be17161e730f16456c5a1c9c9d0444e
Craig D'Souza <craigg.dsouza@mail.utoronto.ca>
2021-07-25 21:34:24 -0400

    Add home content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { FullPage, Slide } from 'react-full-page';
+import { Card } from 'antd';
+import Typing from 'react-typing-animation';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FullPage>
+        <Slide>
+          <div class="main-container">
+            <div class="center">
+            <Typing>
+              <h1 class="main-text">
+                Heyo,
+                <Typing.Delay ms={500} />
+                &nbsp;I'm Craig.
+              </h1>
+              <Typing.Delay ms={2000} />
+              <Typing.Backspace count={17} />
+              <h1 class="main-text">
+                Scroll down to learn about me.
+              </h1>
+            </Typing>
+
+            </div>
+          </div>
+        </Slide>
+        <Slide>
+          <h1>Another slide content</h1>
+        </Slide>
+      </FullPage>
     </div>
   );
 }
