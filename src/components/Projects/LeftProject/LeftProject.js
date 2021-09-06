c27cc20cfd70bc7fb1b0c4fec2c5eea576d27731
Craig D'Souza <craigg.dsouza@mail.utoronto.ca>
2021-09-05 20:58:24 -0400

    Add downward bouncing arrow

--- src/components/Projects/LeftProject/LeftProject.js
+++ src/components/Projects/LeftProject/LeftProject.js
@@ -13,7 +13,7 @@ function LeftProject(props) {
     return (
       <div>
           <div class="full-page">
-          <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} align="middle" >
+          <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 32 }, 48]} align="middle" >
             <Col xs={24} sm={24} md={24} lg={12} xl={12}>
             <LightSpeed left>
               <Image  src={props.image}></Image>
@@ -44,7 +44,16 @@ function LeftProject(props) {
               <Button shape="round" icon={<LinkOutlined />} size="large" href={props.demo}>Live Demo</Button>
               </div>
               </LightSpeed>
+              
             </Col>
+            <Col xs={24} sm={24} md={24} lg={24} xl={24}>
+            <div class="downArrow bounce">
+            <img width="40" height="40" alt="" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/PjwhRE9DVFlQRSBzdmcgIFBVQkxJQyAnLS8vVzNDLy9EVEQgU1ZHIDEuMS8vRU4nICAnaHR0cDovL3d3dy53My5vcmcvR3JhcGhpY3MvU1ZHLzEuMS9EVEQvc3ZnMTEuZHRkJz48c3ZnIGVuYWJsZS1iYWNrZ3JvdW5kPSJuZXcgMCAwIDMyIDMyIiBoZWlnaHQ9IjMycHgiIGlkPSLQodC70L7QuV8xIiB2ZXJzaW9uPSIxLjEiIHZpZXdCb3g9IjAgMCAzMiAzMiIgd2lkdGg9IjMycHgiIHhtbDpzcGFjZT0icHJlc2VydmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiPjxwYXRoIGQ9Ik0yNC4yODUsMTEuMjg0TDE2LDE5LjU3MWwtOC4yODUtOC4yODhjLTAuMzk1LTAuMzk1LTEuMDM0LTAuMzk1LTEuNDI5LDAgIGMtMC4zOTQsMC4zOTUtMC4zOTQsMS4wMzUsMCwxLjQzbDguOTk5LDkuMDAybDAsMGwwLDBjMC4zOTQsMC4zOTUsMS4wMzQsMC4zOTUsMS40MjgsMGw4Ljk5OS05LjAwMiAgYzAuMzk0LTAuMzk1LDAuMzk0LTEuMDM2LDAtMS40MzFDMjUuMzE5LDEwLjg4OSwyNC42NzksMTAuODg5LDI0LjI4NSwxMS4yODR6IiBmaWxsPSIjMTIxMzEzIiBpZD0iRXhwYW5kX01vcmUiLz48Zy8+PGcvPjxnLz48Zy8+PGcvPjxnLz48L3N2Zz4=" />
+          </div>
+            </Col>
+          </Row>
+          <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 32}, 48]}>
+          
           </Row>
           </div>
         </div>
