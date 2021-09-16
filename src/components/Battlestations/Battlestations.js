183dd6a87c11f3e79d17bf564c60e53ab072f555
Craig D'Souza <craigg.dsouza@mail.utoronto.ca>
2021-09-16 17:12:28 -0400

    Alignment issues

--- src/components/Battlestations/Battlestations.js
+++ src/components/Battlestations/Battlestations.js
@@ -3,6 +3,8 @@ import 'react-hoverable-card/dist/index.css'
 
 import { Parallax } from 'react-parallax';
 import { Divider } from 'antd';
+import { Card } from 'antd';
+import { Row, Col } from 'antd';
 import BattlestationGallery from "./BattlestationGallery/BattlestationGallery.js"
 function Battlestations() {
 
@@ -17,7 +19,28 @@ function Battlestations() {
             <BattlestationGallery></BattlestationGallery>
             <br></br>
             <br></br>
-
+            <h1 class="main-title">Current Battlestation</h1>
+            <Row gutter={[10, 60]} justify="space-around">
+              <Col className="gutter-row" xs={24} xl={12}>
+                <Card 
+                  title={<h1>PC</h1>} 
+                  style={{
+                    width: "70%"
+                  }}>
+                  <p>Card content</p>
+                </Card>
+              </Col>
+              <Col className="gutter-row" xs={24} xl={12}>
+                <Card 
+                  title={<h1>Peripherals</h1>} 
+                  style={{
+                    width: "70%"
+                  }}>
+                  <p>Card content</p>
+                </Card>
+              </Col>
+            </Row>
+            
           </div>
         </div>
       </Parallax>
