934e789097bd0afa4b4ed6ca4763136d16dcd286
Craig D'Souza <craigg.dsouza@mail.utoronto.ca>
2021-09-23 21:21:27 -0400

    Add crypto chart

--- src/components/Cryptocurrency/Cryptocurrency.js
+++ src/components/Cryptocurrency/Cryptocurrency.js
@@ -4,30 +4,8 @@ import 'react-hoverable-card/dist/index.css'
 import { Parallax } from 'react-parallax';
 import { Divider } from 'antd';
 import MiningGallery from "./MiningGallery/MiningGallery.js"
-import { PieChart } from 'react-minimal-pie-chart';
+import {PieChart, PieArcSeries, PieArcLabel} from 'reaviz'
 
-export const data = [
-  {
-    key: "Test Data",
-    data: 20
-  },
-  {
-    key: "Test 1",
-    data: 14
-  },
-  {
-    key: "Test 2",
-    data: 5
-  },
-  {
-    key: "Test 3",
-    data: 18
-  },
-  {
-    key: "Test 4",
-    data: 18
-  }
-];
 
 function Cryptocurrency() {
 
@@ -45,9 +23,59 @@ function Cryptocurrency() {
             <br></br>
             <h1 class="main-title">Current Portfolio</h1>
             <p class="subtitle">Here a breakdown of my current cryptocurrency holdings.</p>
-            <div style={{ margin: "55px", textAlign: "center" }}>
-              <PieChart width={250} height={250} data={data} />
-            </div>
+            <PieChart
+              center={true}
+              width={"100%"}
+              height={"100%"}
+              data={
+                [
+                  {
+                    "key": "Dogecoin (DOGE)",
+                    "data": 1.69
+                  },
+                  {
+                    "key": "Bitcoin (BTC)",
+                    "data": 57.87
+                  },
+                  {
+                    "key": "Ethereum (ETH)",
+                    "data": 38.21
+                  },
+                  
+                  {
+                    "key": "Ravencoin (RVN)",
+                    "data": 2.2
+                  },
+                  
+                  {
+                    "key": "QASH (QASH)",
+                    "data": 0.01
+                  },
+                  
+                  {
+                    "key": "Stellar (XLM)",
+                    "data": 0.01
+                  },
+                  
+                ]
+              }
+              series={
+                <PieArcSeries
+                  cornerRadius={4}
+                  padAngle={0.02}
+                  padRadius={200}
+                  doughnut={'true'}
+                  colorScheme={'viridis'}
+                  label={
+                    <PieArcLabel
+                      fontSize={"1.1rem"}
+                      fontFill={"#FFFFFF"}
+                      lineStroke={"rgb(166,173,180)"}
+                    />
+                  }
+                />
+              }
+            />
             <br></br> 
             <br></br>
             <br></br>        
