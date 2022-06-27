70a48ba55a401fd09db8ac044ba638e0fb14c526
Craig D'Souza <cfdt123@gmail.com>
2022-06-26 22:09:51 -0400

    idk

--- src/components/Home/Education/Education.js
+++ src/components/Home/Education/Education.js
@@ -1,6 +1,7 @@
 import { Parallax } from "react-parallax";
 import { Image } from 'antd';
 import degree from '../../../media/degree.png';
+import cka from '../../../media/cka.png';
 
 const styles = {
     textAlign: "center"
@@ -41,15 +42,21 @@ function Interests() {
           }}>
           <div style={insideStyles}>
             <h1 class="main-text">Education</h1>
-            <h1 class="secondary-text">Honours Bachelor of Science, University of Toronto</h1>
-            <h1 class="icon-text">Major: Computer Science</h1>
-            <h1 class="icon-text">Minors: Mathematics, Geography</h1>
+            <h1 class="secondary-text">HBSc. Computer Science - University of Toronto</h1>
+            <h1 class="icon-text">2017-2021</h1>
+            <h1 class="icon-text">High Distinction</h1>
             <h1 class="icon-text">cGPA: 3.54</h1>
             <Image
               width="20rem"
               src={degree}
             />
-
+            {/* <h1 class="main-text">Certifications</h1>
+            <h1 class="secondary-text">Certified Kubernetes Administrator - Linux Foundation</h1>
+            <h1 class="icon-text">April 2022, 94%</h1>
+            <Image
+              width="10rem"
+              src={cka}
+            /> */}
           </div>
         </div>
       </Parallax>
